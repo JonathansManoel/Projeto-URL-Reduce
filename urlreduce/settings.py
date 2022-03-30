dadd1c44ec820dedebad1935bfd697bb1cac79ca
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-03-30 19:01:59 -0300

    Configurando acesso a arquivos estáticos

--- urlreduce/settings.py
+++ urlreduce/settings.py
@@ -141,11 +141,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
-#removida a configuração para rodar o arquivo collectstatic
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     str(BASE_DIR / 'static'),
-# )
+
+#Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    str(BASE_DIR / 'static'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
