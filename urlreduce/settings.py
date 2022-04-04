ec54dbab9bcc2a634ab25bff1b2a10e3ebb03406
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-04-04 16:09:35 -0300

    staticfiles

--- urlreduce/settings.py
+++ urlreduce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fast-mesa-99611.herokuapp.com/']
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
